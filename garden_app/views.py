b15f7ba2b7afeb6f99496acb6a839c58adf2f4b1
Joyce Liao <joyliao07@gmail.com>
2019-02-05 18:04:19 -0800

    re-establishing garden_app routes

--- garden_app/views.py
+++ garden_app/views.py
@@ -6,10 +6,13 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 @login_required
 def weekly_view(request):
     """To render weekly_view with its content."""
-    context = {
-        'temperatures': get_list_or_404(Temperature)
-    }
+    # context = {
+    #     'temperatures': get_list_or_404(Temperature)
+    # }
 
+    context = {
+        'temperatures': [{'has_moisture': 'true', 'date': 'Feb 5'}]
+    }
     return render(request, 'raspberry/weekly.html', context)
 
 
